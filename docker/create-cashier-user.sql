4b55e8e89224721f02b4e53e8b8ef3dd20fee368
nevell <nevell001@gmail.com>
2026-05-03 20:33:56 +0800

    security: remove hardcoded passwords and fix port exposure
    - Remove config/database.properties from git (contains hardcoded password)
    - Remove docker-compose.yml from git (contains hardcoded passwords + 0.0.0.0 binding)
    - Remove docker/create-cashier-user.sql from git (contains example password)
    - Add docker-compose.yml.example template with environment variables
    - Add create-cashier-user.sql.example template
    - Update .gitignore to exclude sensitive config files
    - Fix: bind ports to 127.0.0.1 instead of 0.0.0.0

--- docker/create-cashier-user.sql
+++
@@ -1,5 +0,0 @@
-CREATE USER IF NOT EXISTS 'cashier'@'%' IDENTIFIED WITH mysql_native_password BY 'YourStrongPassword123!';
-GRANT ALL PRIVILEGES ON cashier_system.* TO 'cashier'@'%';
-FLUSH PRIVILEGES;
-SELECT 'Cashier user created successfully!' AS status;
-SELECT user, host, plugin FROM mysql.user WHERE user='cashier';
